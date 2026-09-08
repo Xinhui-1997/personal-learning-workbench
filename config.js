23e8e4729829fb272afb0131c63c882b86c04676
Xinhui-1997 <xinhuizhan1997@163.com>
2026-09-08 14:24:48 +0900

    Connect Supabase client

---
+++ config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://kemjqowxmyzauvbyebwg.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_Y46hSXsY3fRGoVedQ_dCMw_PB4VlaMw",
+  SHOW_DAILY_TASK: true,
+  REQUIRE_LOGIN: true,
+  APP_NAME: "个人学习工作台"
+};
