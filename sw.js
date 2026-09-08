ca98091235efbcd085fbd66db00c9e9d0f104030
Xinhui-1997 <xinhuizhan1997@163.com>
2026-09-08 15:09:56 +0900

    Cache password auth script

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "learning-workbench-v1";
+const CACHE = "learning-workbench-v2";
 const APP_SHELL = [
-  "./", "./index.html", "./styles.css", "./app.js", "./config.js",
+  "./", "./index.html", "./styles.css", "./app.js", "./auth-password.js", "./config.js",
   "./manifest.webmanifest", "./data/demo_cards.json",
   "./icons/icon.svg"
 ];
