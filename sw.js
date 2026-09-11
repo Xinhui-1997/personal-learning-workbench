2fd9adf7572872c637ccd757b252c34f594121de
Xinhui-1997 <xinhuizhan1997@163.com>
2026-09-11 20:10:11 +0900

    Refresh cache for inspiration notes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "learning-workbench-v9";
+const CACHE = "learning-workbench-v10";
 const APP_SHELL = [
-  "./", "./index.html", "./styles.css", "./poetry.css", "./weekend-read.css", "./fun.css", "./auth-password.js", "./ui-enhancements.js", "./meta-cleanup.js", "./fun-module.js", "./config.js",
+  "./", "./index.html", "./styles.css", "./poetry.css", "./weekend-read.css", "./fun.css", "./notes.css", "./auth-password.js", "./ui-enhancements.js", "./meta-cleanup.js", "./fun-module.js", "./notes-module.js", "./config.js",
   "./manifest.webmanifest", "./data/demo_cards.json",
   "./icons/icon.svg"
 ];
